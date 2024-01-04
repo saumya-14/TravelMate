f90de87df8e4a809f2915b8de8bc3391b68eba99
saumya-14 <saumya140904@gmail.com>
2024-01-04 23:26:38 +0530

    Travel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import Login from './components/Login';
 
 function App() {
   const myStorage=window.localStorage;
-  const [currentUser,setCurrentUser]=useState(null);
+  const [currentUser,setCurrentUser]=useState(myStorage.getItem("user"));
   const [pins, setPins] = useState([]);
   const [showPopup, setShowPopup] = useState(true);
   const [currentPlaceId,setcurrentPlaceId]=useState(null);
